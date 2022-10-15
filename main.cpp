4343ade524cde34fa3224a250d4ce693a72870cc
kgul1 <113103268+kgul1@users.noreply.github.com>
2022-10-15 15:32:43 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+float a,b,c;
+cout << "podaj a: ";
+cin >> a;
+cout << "podaj b: ";
+cin >> b;
+c = a * b ;
+cout << "iloczyn wynosi: "<<c;
+
+
+
+
+  return 0;
+}
